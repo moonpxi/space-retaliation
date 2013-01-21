ae87e60d77e334b40ff729c50e122bc7eb309722
moonpxi <paulo.schneider@gmail.com>
2013-01-21 19:57:15 +0000

    Removed unused methods.

--- src/main/java/retaliation/game/entities/Fighter.java
+++ src/main/java/retaliation/game/entities/Fighter.java
@@ -4,18 +4,10 @@ import retaliation.game.shapes.Quad;
 
 public class Fighter {
 
-    private Quad shape;
+    private final Quad shape;
 
     public Fighter(int x, int y) {
         shape = new Quad(x, y, 100, 20);
-    }
-
-    public int getX() {
-        return shape.getX();
-    }
-
-    public int getY() {
-        return shape.getY();
     }
 
     public Quad getShape() {
