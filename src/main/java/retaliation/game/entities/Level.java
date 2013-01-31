a4c8fd2ea78e90eec61136d7478023aa822219e9
moonpxi <paulo.schneider@gmail.com>
2013-01-31 21:41:27 +0000

    Quick hack to make Level block fighter from going past the left side of the screen.

--- src/main/java/retaliation/game/entities/Level.java
+++ src/main/java/retaliation/game/entities/Level.java
@@ -1,16 +1,28 @@
 package retaliation.game.entities;
 
+import retaliation.game.shapes.Shape;
 
-public class Level {
+
+public class Level implements EntityMovementListener {
 
     private final Fighter fighter;
+    private final Shape boundary;
 
     public Level(Fighter fighter) {
         this.fighter = fighter;        
+        boundary = new Shape(0, 0, 800, 600);
+        this.fighter.registerMovementListener(this);
     }
 
     public Fighter getFighter() {
         return fighter;
+    }
+
+    @Override
+    public void entityMoved(Entity entity, int newX, int newY) {
+        if (newX < boundary.getX()) {
+            entity.move(-newX, 0);
+        }        
     }
     
 }
