d62dc6bd7e5c1cae735cc72b76f9cc01d4da4c88
moonpxi <paulo.schneider@gmail.com>
2013-02-22 20:27:37 +0000

    Printing a count of the live entities.

--- src/main/java/retaliation/ui/renderer/LevelRenderer.java
+++ src/main/java/retaliation/ui/renderer/LevelRenderer.java
@@ -16,13 +16,19 @@ public class LevelRenderer implements SlickRenderer {
 
     @Override
     public void render(Graphics graphics) {
+        int count = 0;
+
         for (Entity entity : level.allEntities()) {
+            count++;
+
             Color color = Color.green;
             if (entity.getType() == EntityType.Enemy) {
                 color = Color.white;
             }
             renderMoveable(graphics, entity, color);
         }
+
+        graphics.drawString("Live entities: " + count, 400, 10);
     }
 
     private void renderMoveable(Graphics graphics, Entity entity, Color color) {
