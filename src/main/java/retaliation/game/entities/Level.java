4626cb277e933044cd8dd15ea7222328f421adba
moonpxi <paulo.schneider@gmail.com>
2013-02-24 20:43:19 +0000

    Using Entities to hold entities, instead of a list.

--- src/main/java/retaliation/game/entities/Level.java
+++ src/main/java/retaliation/game/entities/Level.java
@@ -1,6 +1,5 @@
 package retaliation.game.entities;
 
-import com.google.common.base.Function;
 import retaliation.game.entities.listener.SpaceshipShootingListener;
 import retaliation.game.geometry.Position;
 import retaliation.game.logic.EnemyAI;
@@ -12,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.collect.Iterables.concat;
-import static com.google.common.collect.Iterables.transform;
 import static java.util.Arrays.asList;
 import static retaliation.game.entities.EntityType.Laser;
 import static retaliation.game.geometry.Dimension.size;
@@ -23,31 +21,30 @@ public class Level implements SpaceshipShootingListener {
     private PlayerShipControls player;
     private final List<EnemyAI> enemies = new ArrayList<EnemyAI>();
     private final List<FlyingLaser> lasers = new ArrayList<FlyingLaser>();
+    private final Entities entities = new Entities();
 
     public Level(Spaceship playerShip) {
         this.player = new PlayerShipControls(playerShip);
 
         playerShip.registerShootingListener(this);
+
+        entities.add(playerShip);
     }
 
     public void addEnemyShip(Spaceship enemyShip) {
         enemies.add(new EnemyAI(enemyShip));
+        entities.add(enemyShip);
     }
 
     @Override
     public void fired(Position from) {
-        lasers.add(new FlyingLaser(new Entity(Laser, from, size(1, 3))));
+        Entity laser = new Entity(Laser, from, size(1, 3));
+        lasers.add(new FlyingLaser(laser));
+        entities.add(laser);
     }
 
     public Iterable<? extends Entity> allEntities() {
-        Iterable<Spaceship> enemyShips = transform(this.enemies, new Function<EnemyAI, Spaceship>() {
-            @Override public Spaceship apply(EnemyAI enemy) { return enemy.getShip(); }
-        });
-        Iterable<Entity> laserBolts = transform(lasers, new Function<FlyingLaser, Entity>() {
-            @Override public Entity apply(FlyingLaser laser) { return laser.getLaser(); }
-        });
-
-        return concat(asList(player.getShip()), enemyShips, laserBolts);
+        return entities.activeEntities();
     }
 
     public Iterable<GameLogic> allEntitiesLogic() {
--- src/test/java/retaliation/game/entities/LevelTest.java
+++ src/test/java/retaliation/game/entities/LevelTest.java
@@ -1,43 +1,11 @@
 package retaliation.game.entities;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
-import static retaliation.game.entities.EntityType.*;
-import static retaliation.game.geometry.Dimension.size;
-import static retaliation.game.geometry.Position.at;
-
 public class LevelTest {
-    private final Level level = new Level(new Spaceship(Player, at(20, 40), size(100, 100)));
 
-    @SuppressWarnings("unchecked")
     @Test public void
-    returnsAllEntities() {
-        level.fired(at(25, 35));
-        level.addEnemyShip(new Spaceship(Enemy, at(30, 40), size(100, 20)));
+    toDo() {
 
-        assertThat(level.allEntities(), contains(isEntity(Player, 20, 40),
-                                                 isEntity(Enemy, 30, 40),
-                                                 isEntity(Laser, 25, 35)));
-    }
-
-    private Matcher<Entity> isEntity(final EntityType type, final float x, final float y) {
-        return new TypeSafeDiagnosingMatcher<Entity>() {
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendValue("Expected " + type + " at " + x + ", " + y);
-            }
-
-            @Override
-            protected boolean matchesSafely(Entity actual, Description description) {
-                description.appendValue("Entity was a " + actual.getType() + " at " + actual.position().x() + ", " + actual.position().y());
-                return actual.getType() == type && actual.position().equals(at(x, y));
-            }
-        };
     }
 }
