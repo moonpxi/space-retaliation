da9e6cea5933d58b52c81d72a204c95725218f05
moonpxi <paulo.schneider@gmail.com>
2013-03-07 19:59:59 +0000

    Test cases for lasers damaging ships.

--- src/test/java/retaliation/game/rules/LasersDamageShipsRuleTest.java
+++ src/test/java/retaliation/game/rules/LasersDamageShipsRuleTest.java
@@ -12,6 +12,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static retaliation.game.entities.EntityType.Enemy;
 import static retaliation.game.entities.EntityType.Laser;
+import static retaliation.game.entities.Spaceship.State.Alive;
 import static retaliation.game.entities.Spaceship.State.Destroyed;
 import static retaliation.game.geometry.Dimension.size;
 import static retaliation.game.geometry.Position.at;
@@ -28,6 +29,30 @@ public class LasersDamageShipsRuleTest {
         new LasersDamageShipsRule().apply(entities);
 
         assertThat(ship.state(), is(Destroyed));
+    }
+
+    @Test public void
+    laserMissesAShip() {
+        laser(at(20, 20));
+        Spaceship ship = ship(at(30, 30), size(50, 50));
+
+        new LasersDamageShipsRule().apply(entities);
+
+        assertThat(ship.state(), is(Alive));
+    }
+
+    @Test public void
+    aFewShipsHitAndMissed() {
+        laser(at(10, 10)); laser(at(20, 10)); laser(at(50, 55));
+        Spaceship hit = ship(at(10, 15), size(30, 30));
+        Spaceship alsoHit = ship(at(43, 10), size(100, 100));
+        Spaceship missed = ship(at(100, 105), size(30, 30));
+
+        new LasersDamageShipsRule().apply(entities);
+
+        assertThat(hit.state(), is(Destroyed));
+        assertThat(alsoHit.state(), is(Destroyed));
+        assertThat(missed.state(), is(Alive));
     }
 
     private Entity laser(Position position) {
